5d3a4403caf1d1088b4e5bfdd6b27220854edac8
nico212318 <151861323+nico212318@users.noreply.github.com>
2023-11-23 14:35:54 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,74 @@
 from PyQt5 import QtWidgets, uic
-import sys
 import sympy as sp
+import sys
 
+app = QtWidgets.QApplication([])
+inicio = uic.loadUi("Proyecto1.ui")
+entrar = uic.loadUi("U3.ui")
+error = uic.loadUi("U2.ui")
+registro = uic.loadUi("U1.ui")
 x = sp.symbols('x')
 
-class Ui(QtWidgets.QMainWindow):
-    def _init_(self):
-        super(Ui, self)._init_()
-        uic.loadUi('U3.ui', self)
-        self.calculo.clicked.connect(self.factoreo)
-        self.show()
+def gui_login():
+    usser = inicio.lineEdit.text()
+    password = inicio.lineEdit_2.text()
+    if len(usser)==0 or len(password)==0:
+        inicio.label_2.setText("ENTER DATE")
+    elif usser == "nf833949@gmail.com" and password == "2123":
+        gui_entrar()
+    else:
+        gui_error()
 
-    def factoreo(self):
-        expresion = self.ejercicio.text()
+def gui_contacto():
+    nombre = registro.lineEdit.text()
+    apellido= registro.lineEdit_2.text()
+    email= registro.lineEdit_3.text()
+    contrasena= registro.lineEdit_4.text()
+    if len(nombre)==0 or len(apellido)==0 or len(email)== 0 or len(contrasena)==0:
+        registro.dato.setText("Enter Date")
+    else:
+        registro.dato.setText("  ")
+        registro.dato_2.setText("data recorded")
+
+
+
+def factoreo():
+        expresion = entrar.ejercicio.text()
+        ejercicio.factores.connect()
         factores = sp.factor(expresion)
-        self.imprimir_resultado(factores)
+        print(factores)
 
-    def imprimir_resultado(self, resultado):
-        self.resultado.setText(str(resultado))
+def gui_entrar():
+        inicio.hide()
+        entrar.show()
+def gui_error():
+    inicio.hide()
+    error.show()
+def regresar_entrar():
+    entrar.hide()
+    inicio.show()
 
-app = QtWidgets.QApplication(sys.argv)
-window = Ui()
-app.exec_()
+
+def gui_registro():
+    inicio.hide()
+    registro.show()
+
+
+def regresar_login():
+        registro.hide()
+        inicio.show()
+def regresar_error():
+        error.hide()
+        inicio.show()
+
+
+inicio.pushButton.clicked.connect(gui_login)
+entrar.pushButton.clicked.connect(regresar_entrar)
+error.pushButton.clicked.connect(regresar_error)
+inicio.commandLinkButton.clicked.connect(gui_registro)
+registro.pushButton_2.clicked.connect(regresar_login)
+entrar.calculo.clicked.connect(factoreo)
+registro.pushButton.clicked.connect(gui_contacto)
+
+inicio.show()
+app.exec()
